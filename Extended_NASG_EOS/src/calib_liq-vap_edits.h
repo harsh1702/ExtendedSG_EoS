14960d485e04c9e8b18594d94badec5ec4329f9b
Harsh Kulkarni <48854316+harsh1702@users.noreply.github.com>
2020-05-19 17:52:21 +0530

    Update calib_liq-vap_edits.h

--- Extended_NASG_EOS/src/calib_liq-vap_edits.h
+++ Extended_NASG_EOS/src/calib_liq-vap_edits.h
@@ -8,13 +8,22 @@
 #include <cmath>
 
 // Reading experimental data files
-void readLiqVapInput(double &p0, double &ro0, double &c0);
+void readLiqVapInput(double &p0L, double &ro0L, double &c0L, double &p0G, double &ro0G, double &coG);
 
 // --- Vapor phase ---
 double computecpG(std::vector<double> const& hGexp, std::vector<double> const& ThGexp);
 double computeQg(std::vector<double> const& hGexp, std::vector<double> const& ThGexp, double cpG);
 double computecvG(std::vector<double> const& vGexp, std::vector<double> const& Texp, std::vector<double> const& psatExp, double cpG);
-double computeQprimG(std::vector<double> p, std::vector<double> T, double cpL, double cpG, double cvL, double cvG, double qL, double qG, double pinfL, double bL);
+double computeQprimG(std::vector<double> p, std::vector<double> T, double cpL, double cpG, double cvL, double cvG, double qL, double qG, double pinfL, double pinfG, double bL);
+//modifications to vapour phase
+double computebG(double mvG, double mTp, double diffC);
+double computeHeatCapDiffG(std::vector<double> const& psatExp, std::vector<double> const& Texp, std::vector<double> const& vLexp, double mvL, double mTp, double pinfG);
+double computeDHeatCapDiffG(std::vector<double> const& psatExp, std::vector<double> const& Texp, std::vector<double> const& vGexp, double mvG, double mTp, double mTp2, double pinfG);
+double computeCpG(std::vector<double> const& Texp, std::vector<double> const& hGexp, std::vector<double> const& psatExp, double mhG, double mp, double mT, double bG);
+double computePinfG(std::vector<double> const& psatExp, std::vector<double> const& Texp, std::vector<double> const& vGexp, std::vector<double> const& hGexp, double p0, double ro0, double c0);
+double computeDbG(double mTp, double mTp2, double diffC, double dDiffC);
+double computeDcpG(std::vector<double> const& Texp, std::vector<double> const& psatExp, double mp, double mT, double dbG);
+
 
 // --- Liquid phase ---
 double computeMeanTp(std::vector<double> const& psatExp, std::vector<double> const& Texp, double pinfL);
